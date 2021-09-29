f52d6c2d5adba375d91bcbbcfdbc76d043f74834
akashVerma <akashv000@gmail.com>
2021-09-30 01:28:06 +0530

    sanshu

--- routers/subscription.js
+++ routers/subscription.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const { subsById, create, listByUser } = require('../controllers/subscription')
 
+router.post('/subs/create', create)
+router.get('/subs/list', listByUser)
 
-
-
+router.param('subsId', subsById)
 module.exports = router
