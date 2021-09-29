4c69e5701c838a351ca1e81bb3e90f427e2fa41b
akashVerma <akashv000@gmail.com>
2021-09-29 13:53:45 +0530

    LATEST CHANGES

--- controllers/queries.js
+++ controllers/queries.js
@@ -15,17 +15,79 @@ exports.feedbackCreate = (req, res) => {
         message: message
     })
 
-    feedback.save((err, data) => {
+    Feedback.find({ email: email }).exec((err, data) => {
+        if (err) {
+            return res.status(400).json({ error: err });
+        } else {
+            console.log(data)
+            if (data.length !== 0) {
+                return res.json({ message: 'Your Already Give feedback' });
+            } else {
+                feedback.save((err, data) => {
+                    if (err) {
+                        return res.status(400).json({ error: err });
+                    }
+                    return res.json({
+                        message: 'Feedback Added',
+                        data: data
+                    });
+                })
+            }
+        }
+    })
+
+}
+
+exports.queriesCreate = (req, res) => {
+    const { name, email, phone, issues, user } = req.body
+    const query = new Queries({
+        name: name,
+        email: email,
+        phone: phone,
+        issues: issues,
+        user: user
+    })
+    query.save((err, data) => {
         if (err) {
             return res.status(400).json({ error: err });
         }
         return res.json({
-            message: 'Feedback Added',
+            message: 'Query Added Successfully !',
             data: data
         });
     })
 }
 
+exports.cancelationCreate = (req, res) => {
+    const { name, email, phone, issues, user, orderId } = req.body
+    const query = new Cancellation({
+        name: name,
+        email: email,
+        phone: phone,
+        issues: issues,
+        user: user,
+        orderId: orderId
+    })
+    Cancellation.find({ orderId: orderId }).exec((err, dataa) => {
+        if (err) {
+            return res.status(400).json({ error: err });
+        } else {
+            if (dataa.length === 0) {
+                query.save((err, data) => {
+                    if (err) {
+                        return res.status(400).json({ error: err });
+                    }
+                    return res.json({
+                        message: 'Cancellation Added Successfully !',
+                        data: data
+                    });
+                })
+            } else {
+                return res.json({ message: 'You Already Requested !!!' });
+            }
+        }
+    })
+}
 
 exports.feedbackList = (req, res) => {
     Feedback.find()
--- routers/cancellation.js
+++ routers/cancellation.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const { cancelationCreate } = require('../controllers/queries')
 
-
-
+router.post('/create/cancellation', cancelationCreate);
 
 module.exports = router
