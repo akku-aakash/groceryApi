777259eae39a7c0c2c40f987350cddc9dbbde9b9
akashVerma <akashv000@gmail.com>
2021-10-01 14:06:47 +0530

    hello

--- routers/queries.js
+++ routers/queries.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const { queriesCreate } = require('../controllers/queries')
 
-
-
+router.post('/create/report', queriesCreate)
 
 module.exports = router
